bb5619ec3e05e1a06cd8db869569c704c807bd3c
Rodney Mupanduki <rodneym@afrosoft.co.zw>
2022-02-06 10:20:59 -0800

    refactoring + enviroment variables

--- apps/admin/src/app/app.module.ts
+++ apps/admin/src/app/app.module.ts
@@ -9,8 +9,8 @@ import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { ShellComponent } from './shared/shell/shell.component';
 import { SidebarComponent } from './shared/sidebar/sidebar.component';
-import { CategoriesListComponent } from './categories/categories-list/categories-list.component';
-import { CategoriesFormComponent } from './categories/categories-form/categories-form.component';
+import { CategoriesListComponent } from './pages/categories/categories-list/categories-list.component';
+import { CategoriesFormComponent } from './pages/categories/categories-form/categories-form.component';
 
 import { CategoriesService } from '@bluebits/products';
 import { ConfirmationService, MessageService } from 'primeng/api';
--- apps/admin/src/app/pages/categories/categories-form/categories-form.component.ts
+++ apps/admin/src/app/pages/categories/categories-form/categories-form.component.ts
@@ -83,6 +83,10 @@ export class CategoriesFormComponent implements OnInit {
     })
   }
 
+  onCancel(){
+    this.location.back();
+  }
+
   get categoryForm(){
     return this.form.controls
   }
